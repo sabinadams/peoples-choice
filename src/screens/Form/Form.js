2ff3c4ab73211450b2af3c8c8d9d051c38f5394b
Sabin <sabintheworld@gmail.com>
2019-09-16 11:02:22 -0700

    Form Updates

--- src/screens/Form/Form.js
+++ src/screens/Form/Form.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react'
 import './Form.scss';
 import { withFirebase } from '../../Firebase'
 import Loading from '../Loading/Loading'
+import PointSymbol from '../../components/PointSymbol/PointSymbol'
+import _ from 'lodash';
 
 function Form(props) {
     const [ question, setQuestion ] = useState(null)
+
+    const [ answers, setAnswers ] = useState([null, null, null])
+
+    const [ formComplete, setFormComplete ] = useState(false)
 
     useEffect( () => {
         let unsubscribe = props.firebase
@@ -13,28 +19,59 @@ function Form(props) {
             .onSnapshot( snapshot => snapshot.forEach( doc => setQuestion({ ...doc.data(), id: doc.id }) ) )
             
         return function cleanup() { unsubscribe() }
-    }, [])
+    }, [props.firebase])
 
-    const FormItem = () => (
-        <select>
-            {
-                Object.keys(question.options).map( key => (
-                    <option key={key} value={key}>{ question.options[key] }</option>
-                ))
-            }
-        </select>
+    useEffect( () => {
+        setFormComplete( answers[1] && answers[2] && answers[3] )  
+    }, [answers])
+
+    const changeAnswer = (index, value) => {
+        console.log('Changing Answer')
+        let newAnswers = answers
+        newAnswers[index] = value
+        setAnswers(newAnswers)
+
+        console.log(answers)
+    }
+
+    const FormItem = props => (
+        <div className="formFieldContainer">
+            <div className="pointSymbols">
+                { _.times(props.points, index => <PointSymbol key={index}/>) }
+            </div>
+            
+            <select defaultValue={null} className="formField" onChange={event => changeAnswer(props.index, event.target.value)}>
+                <option disabled value={null} hidden>{props.message} choice...</option>
+                { 
+                    Object.keys(question.options).map( key => 
+                        !answers.includes(key.toString()) ? 
+                            <option key={key} value={key}>{ question.options[key] }</option> 
+                            : null
+                    ) 
+                }
+            </select>
+        </div>
     )
+
+    // const validateForm()
+    const handleSubmit = () => {
+        // if ( validateForm() ) {
+
+        // }
+    }
 
 	return question ? (
         <div id="formPage">
-            <h2>{question.message}</h2>
-
-            <form>
-                <FormItem />
-                <FormItem />
-                <FormItem />
-            </form>
-            
+            <h2 className="question">{question.message}</h2>
+            <hr className="divider"/>
+            <div className="formContainer">
+                <FormItem index={0} message='First' points={3}/>
+                <FormItem index={1} message='Second' points={2}/>
+                <FormItem index={2} message='Third' points={1}/>
+            </div>
+            <hr className="divider"/>
+            <br />
+            <button disabled={formComplete} onClick={() => handleSubmit()} className="submitButton">Vote</button>
 		</div>
 	) : <Loading message="Loading Question"/>
 }
